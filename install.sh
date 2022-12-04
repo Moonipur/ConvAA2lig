cdc69bf06b08a6e7f3f5eb8adfe2dc3554043e7c
Moonipur <119776865+Moonipur@users.noreply.github.com>
2022-12-04 13:34:16 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ -f ~/.bash_profile ];
 then
    Loc=`find "$(cd ..; pwd)" -name "ConvAA2lig.py"`;
    echo "alias ConvAA2lig='python3 ${Loc}'" >> ~/.bash_profile;
+   source ~/.bash_profile;
 else
    echo "Your .bash_profile does NOT exist";
 fi
