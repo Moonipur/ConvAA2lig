dc812b91d234cedd6f3f8e2d1ee083df6e6cba14
Moonipur <119776865+Moonipur@users.noreply.github.com>
2022-12-05 13:11:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ -f ~/.bashrc ];
 then
    Loc=`find "$(cd ..; pwd)" -name "ConvAA2lig.py"`;
    echo "alias ConvAA2lig='python3 ${Loc}'" >> ~/.bashrc;
+   echo "Installation already finished";
 else
    echo "Your .bashrc does NOT exist";
 fi
