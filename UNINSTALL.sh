53df31c1959f4a6d7560c0aefd895e6d6f19f557
Moonipur <119776865+Moonipur@users.noreply.github.com>
2022-12-08 10:37:14 +0700

    Update UNINSTALL.sh

--- UNINSTALL.sh
+++ UNINSTALL.sh
@@ -4,6 +4,6 @@ comm=`find "$(cd ..; pwd)" -name "ConvAA2lig.py"`
 
 Loc=`grep -n "alias ConvAA2lig='python3 ${comm}'" ~/.bashrc | cut -b 1-3`
 
-sed -i sed -i "${Loc}d" ~/.bashrc ~/.bashrc 
+sed -i "${Loc}d" ~/.bashrc 
 
 echo "Uninstallation is already finished"
